ebe147619e42b450232dfb0068b1bc261866187d
Daniel Coto <danielcotov@gmail.com>
2021-03-26 17:37:10 -0600

    NEW PROJECT

---
+++ app/src/main/java/com/sc703/gualmarsh/MainActivity.java
@@ -0,0 +1,32 @@
+package com.sc703.gualmarsh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        TimerTask timerTask = new TimerTask(){
+
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomeLoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        Timer time = new Timer();
+        time.schedule(timerTask,2500);
+    }
+}
---
+++ app/src/main/java/com/sc703/gualmarsh/dashboard/DashboardActivity.java
@@ -0,0 +1,31 @@
+package com.sc703.gualmarsh.dashboard;
+
+import android.os.Build;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.sc703.gualmarsh.R;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+    }
+}
