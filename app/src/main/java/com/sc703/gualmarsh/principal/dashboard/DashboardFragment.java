d81890a96def3e0c75ae75b7c360a21736689420
Daniel Coto <danielcotov@gmail.com>
2021-03-29 20:22:07 -0600

    Bottom navigation view Dashboard finished

--- app/src/main/java/com/sc703/gualmarsh/principal/dashboard/DashboardFragment.java
+++ app/src/main/java/com/sc703/gualmarsh/principal/dashboard/DashboardFragment.java
@@ -1,6 +1,7 @@
 package com.sc703.gualmarsh.principal.dashboard;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,6 +11,7 @@ import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -23,6 +25,11 @@ public class DashboardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        if(Build.VERSION.SDK_INT >= 21){
+            Window window = getActivity().getWindow();
+            window.setStatusBarColor(getActivity().getResources().getColor(R.color.w_darkBG));
+        }
 
         /*Button btnSignOut = root.findViewById(R.id.btn_sign_out);
         Button btnWeb = root.findViewById(R.id.btn_web);
