66483f1a23bf9527db3d87720097483842dae457
deathstars22 <josgabfer@outlook.com>
2021-04-01 14:18:03 -0600

    Working Web Fragment

--- app/src/main/java/com/sc703/gualmarsh/principal/dashboard/SettingsFragment.java
+++ app/src/main/java/com/sc703/gualmarsh/principal/dashboard/SettingsFragment.java
@@ -1,6 +1,12 @@
 package com.sc703.gualmarsh.principal.dashboard;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,7 +16,10 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.sc703.gualmarsh.R;
@@ -23,9 +32,10 @@ public class SettingsFragment extends Fragment {
         super.onCreateView(inflater, container, savedInstanceState);
 
         View root = inflater.inflate(R.layout.fragment_settings, container, false);
-
+        SolicitarPermisos();
         Button btnSignOut = root.findViewById(R.id.btn_logOut);
-//        Button btnWeb = root.findViewById(R.id.btn_web);
+        Button btnWeb = root.findViewById(R.id.btn_Web);
+        Button btnPhone = root.findViewById(R.id.btn_Phone);
 
         btnSignOut.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,13 +46,39 @@ public class SettingsFragment extends Fragment {
                 startActivity(intent);
             }
         });
-//        btnWeb.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                NavController navController = Navigation.findNavController(getActivity(), R.id.nav_principal_fragment);
-//                navController.navigate(R.id.action_Dashboard_to_Web);
-//            }
-//        });*/
+        btnWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_settings_to_Web);
+
+            }
+        });
+        btnPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel: 800-8000-722  "));
+                startActivity(intent);
+            }
+        });
+
+
         return root;
+    }
+    private void SolicitarPermisos(){
+        final int PERMISSION_CODE = 55895;
+        int Permiso_GPS = ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION);
+        int Permiso_Telefono = ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CALL_PHONE);
+        int Permiso_Internet = ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.INTERNET);
+
+        if(Permiso_GPS != PackageManager.PERMISSION_GRANTED
+                || Permiso_Telefono != PackageManager.PERMISSION_GRANTED
+                || Permiso_Internet != PackageManager.PERMISSION_GRANTED){
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                requestPermissions(new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.CALL_PHONE,
+                        Manifest.permission.INTERNET},PERMISSION_CODE);
+            }
+        }
     }
 }
