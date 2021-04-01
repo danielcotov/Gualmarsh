c61c5152e12b90d5fe99a449af9cc7e686ac1c19
deathstars22 <josgabfer@outlook.com>
2021-03-31 23:12:01 -0600

    Working log out on settings fragment

---
+++ app/src/main/java/com/sc703/gualmarsh/principal/dashboard/SettingsFragment.java
@@ -0,0 +1,48 @@
+package com.sc703.gualmarsh.principal.dashboard;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.sc703.gualmarsh.R;
+import com.sc703.gualmarsh.login.LoginActivity;
+
+public class SettingsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        View root = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        Button btnSignOut = root.findViewById(R.id.btn_logOut);
+//        Button btnWeb = root.findViewById(R.id.btn_web);
+
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(getActivity(), R.string.signed_out, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+//        btnWeb.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                NavController navController = Navigation.findNavController(getActivity(), R.id.nav_principal_fragment);
+//                navController.navigate(R.id.action_Dashboard_to_Web);
+//            }
+//        });*/
+        return root;
+    }
+}
