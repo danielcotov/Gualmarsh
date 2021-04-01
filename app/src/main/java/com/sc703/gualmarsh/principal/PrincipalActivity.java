f9cfbd1264a9da0dd2a0c71c6108e5df8e5d195a
deathstars22 <josgabfer@outlook.com>
2021-03-31 22:23:02 -0600

    Working log out on settings fragment

--- app/src/main/java/com/sc703/gualmarsh/principal/PrincipalActivity.java
+++ app/src/main/java/com/sc703/gualmarsh/principal/PrincipalActivity.java
@@ -1,6 +1,5 @@
 package com.sc703.gualmarsh.principal;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,13 +8,7 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.sc703.gualmarsh.login.LoginActivity;
 import com.sc703.gualmarsh.R;
 
 public class PrincipalActivity extends AppCompatActivity {
@@ -24,7 +17,6 @@ public class PrincipalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
-
         BottomNavigationView navView = findViewById(R.id.bottomNav_view);
 
 
@@ -36,16 +28,6 @@ public class PrincipalActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(PrincipalActivity.this, R.id.nav_principal_fragment);
         NavigationUI.setupWithNavController(navView, navController);
 
-        /*Button btnSignOut = findViewById(R.id.btn_sign_out);
 
-        btnSignOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Toast.makeText(PrincipalActivity.this, R.string.signed_out, Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(PrincipalActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });*/
     }
 }
