112e496bbdce342c039b66501815ab2c95d65520
deathstars22 <josgabfer@outlook.com>
2021-04-12 19:11:19 -0600

    Non working Storage :)

--- app/src/main/java/com/sc703/gualmarsh/database/models/product/ProductAdapter.java
+++ app/src/main/java/com/sc703/gualmarsh/database/models/product/ProductAdapter.java
@@ -1,10 +1,15 @@
 package com.sc703.gualmarsh.database.models.product;
 
 
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -12,13 +17,26 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.sc703.gualmarsh.R;
 import com.sc703.gualmarsh.principal.inventory.ItemClickListener;
+
+import java.io.File;
+import java.io.IOException;
 
 public class ProductAdapter extends FirebaseRecyclerAdapter<Product, ProductAdapter.Holder>  {
 
     private ItemClickListener itemClickListener;
     private GridLayoutManager gridLayoutManager;
+    private StorageReference storage;
+    private ImageView imvImage;
+    private Uri ImageURI;
+    private final Integer CODE= 1234;
+
 
     public ProductAdapter(@NonNull FirebaseRecyclerOptions<Product> options, GridLayoutManager gridLayoutManager)
     {
@@ -46,16 +64,18 @@ public class ProductAdapter extends FirebaseRecyclerAdapter<Product, ProductAdap
 
         public Holder(View item, int viewType){
             super(item);
-
             if (viewType != 1) {
+                imvImage = item.findViewById(R.id.imv_gridItemPhoto);
                 tvProductCode = item.findViewById(R.id.tv_grid_code);
                 tvProductName = item.findViewById(R.id.tv_grid_name);
                 tvProductQuantity = item.findViewById(R.id.tv_grid_quantity);
             }else{
+                imvImage = item.findViewById(R.id.imv_list_itemPhoto);
                 tvProductCode = item.findViewById(R.id.tv_list_code);
                 tvProductName = item.findViewById(R.id.tv_list_name);
                 tvProductQuantity = item.findViewById(R.id.tv_list_quantity);
             }
+
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -80,9 +100,29 @@ public class ProductAdapter extends FirebaseRecyclerAdapter<Product, ProductAdap
 
     @Override
     public void onBindViewHolder(@NonNull ProductAdapter.Holder holder, int position, @NonNull Product model) {
-
         holder.tvProductCode.setText(model.getCode());
         holder.tvProductName.setText(String.format(String.valueOf(model.getName())));
+
+        storage = FirebaseStorage.getInstance().getReference().child("Imagenes/"+ model.getCode());
+        File tempFile = null;
+        try {
+            tempFile = File.createTempFile("image", "jpg");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }final File finalFile = tempFile;
+        storage.getFile(tempFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                String file = finalFile.getAbsolutePath();
+                imvImage.setImageBitmap(BitmapFactory.decodeFile(file));
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e("Tag", "Image not found");
+            }
+        });
+
         if (model.getQuantity() != null) {
             holder.tvProductQuantity.setText(model.getQuantity().toString());
         }else{
