2926e48c055fffad9255f127c7510732312d7057
Daniel Coto <danielcotov@gmail.com>
2021-04-12 21:37:03 -0600

    Show Item Close button

--- app/src/main/java/com/sc703/gualmarsh/principal/inventory/ShowItemFragment.java
+++ app/src/main/java/com/sc703/gualmarsh/principal/inventory/ShowItemFragment.java
@@ -1,27 +1,37 @@
 package com.sc703.gualmarsh.principal.inventory;
 
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.sc703.gualmarsh.R;
 
 public class ShowItemFragment extends Fragment {
+
+    private ImageView imvClose;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_show_item, container, false);
 
-
-
-
+        imvClose = root.findViewById(R.id.showItem_Close);
+        imvClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavController navController = Navigation.findNavController(getActivity(), R.id.nav_principal_fragment);
+                navController.navigate(R.id.action_Show_to_Products);
+            }
+        });
 
         return root;
-
     }
 }
