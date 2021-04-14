a3ae85bf4a3c8dc285de59a13221fbbd1a22e879
deathstars22 <josgabfer@outlook.com>
2021-04-14 11:26:45 -0600

    Working Categories and Total Quantity widgets in dashboard, pending total value and Items

--- app/src/main/java/com/sc703/gualmarsh/principal/dashboard/DashboardFragment.java
+++ app/src/main/java/com/sc703/gualmarsh/principal/dashboard/DashboardFragment.java
@@ -4,27 +4,56 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.sc703.gualmarsh.R;
 import com.sc703.gualmarsh.login.LoginActivity;
 
 public class DashboardFragment extends Fragment {
+    TextView itemsNum;
+    TextView categoriesNum;
+    TextView dashboardQuantity;
+    TextView dashboardTotal;
+    private FirebaseDatabase FBDB = FirebaseDatabase.getInstance();
+    private DatabaseReference BDref = FBDB.getReference();
+    private DatabaseReference categories = BDref.child("categories");
+    private DatabaseReference productsTotal = BDref.child("products");
+    private DatabaseReference productsCatTotal = BDref.child("productCategories");
+
+
+    long categoriesTemp;
+    long productsTotalTemp;
+    long productsCatTotalTemp;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        itemsNum = root.findViewById(R.id.tv_dashboardItemsNum);
+        categoriesNum = root.findViewById(R.id.tv_dashboardCategoriesNum);
+        dashboardQuantity = root.findViewById(R.id.tv_dashboardQuantityNum);
+        dashboardTotal = root.findViewById(R.id.tv_dashboardTotalNum);
 
         if(Build.VERSION.SDK_INT >= 21){
             Window window = getActivity().getWindow();
@@ -32,5 +61,56 @@ public class DashboardFragment extends Fragment {
         }
 
         return root;
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        categories.addListenerForSingleValueEvent (new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                String textCategories = String.valueOf(snapshot.getChildrenCount());
+                categoriesTemp = snapshot.getChildrenCount();
+
+                categoriesNum.setText(textCategories);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                String text = "Unable to establish a connection to the DB";
+                categoriesNum.setText(text);
+            }
+        });
+        productsTotal.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                productsCatTotalTemp = snapshot.getChildrenCount();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        productsCatTotal.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                productsCatTotalTemp = snapshot.getChildrenCount();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        long totalQuantity = productsCatTotalTemp + productsTotalTemp + categoriesTemp;
+
+        String total = String.valueOf(totalQuantity);
+
+        dashboardTotal.setText(total);
+
+
+
     }
 }
