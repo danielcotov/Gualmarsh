188ae033223310fa4bb683321f1579f6080f9998
Daniel Coto <danielcotov@gmail.com>
2021-04-17 17:31:35 -0600

    Settings fragment updated with language change

--- app/src/main/java/com/sc703/gualmarsh/principal/dashboard/SettingsFragment.java
+++ app/src/main/java/com/sc703/gualmarsh/principal/dashboard/SettingsFragment.java
@@ -1,40 +1,22 @@
 package com.sc703.gualmarsh.principal.dashboard;
 
-import android.Manifest;
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.res.Resources;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.sc703.gualmarsh.LocaleHelper;
-import com.sc703.gualmarsh.MainActivity;
 import com.sc703.gualmarsh.R;
 import com.sc703.gualmarsh.login.LoginActivity;
-import com.sc703.gualmarsh.principal.inventory.ItemViewModel;
-
-import java.util.Locale;
 
 public class SettingsFragment extends Fragment {
 
