fdac325cc64e7b7cb3ea785a5099d88d347d8722
deathstars22 <josgabfer@outlook.com>
2021-04-17 22:31:07 -0600

    Possible working import export in dashboard

--- app/src/main/java/com/sc703/gualmarsh/principal/dashboard/DashboardFragment.java
+++ app/src/main/java/com/sc703/gualmarsh/principal/dashboard/DashboardFragment.java
@@ -1,23 +1,30 @@
 package com.sc703.gualmarsh.principal.dashboard;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.os.FileUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -32,8 +39,15 @@ import com.sc703.gualmarsh.R;
 import com.sc703.gualmarsh.login.LoginActivity;
 import com.sc703.gualmarsh.principal.inventory.ItemViewModel;
 
+import org.json.*;
+
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Map;
+import java.util.Calendar;
 
 public class DashboardFragment extends Fragment {
     TextView itemsNum;
@@ -44,13 +58,20 @@ public class DashboardFragment extends Fragment {
     private DatabaseReference BDref = FBDB.getReference();
     private DatabaseReference categoriesref = BDref.child("categories");
     private DatabaseReference allProducts = BDref.child("products");
+    private DatabaseReference productCategories = BDref.child("productCategories");
     private Long count = Long.parseLong("0");
-    private Long count2 = Long.parseLong("0");
+    String counter = "0";
     private ItemViewModel viewModel;
     private int sum;
     private int sum2;
+    String fullDB;
+    Calendar calendar = Calendar.getInstance();
+    final int year = calendar.get(Calendar.YEAR);
+    final int month = calendar.get(Calendar.MONTH);
+    final int day = calendar.get(Calendar.DAY_OF_MONTH);
 
-        @Override
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
@@ -58,6 +79,7 @@ public class DashboardFragment extends Fragment {
         categoriesNum = root.findViewById(R.id.tv_dashboardCategoriesNum);
         dashboardQuantity = root.findViewById(R.id.tv_dashboardQuantityNum);
         dashboardTotal = root.findViewById(R.id.tv_dashboardTotalNum);
+        ImageView exportButton = root.findViewById(R.id.btn_export);
 
         if(Build.VERSION.SDK_INT >= 21){
             Window window = getActivity().getWindow();
@@ -76,18 +98,17 @@ public class DashboardFragment extends Fragment {
 
             }
         });
-
+        exportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                exportDB();
+            }
+        });
 
         return root;
     }
-    @Override
-    public void onStart() {
-        super.onStart();
 
-
-    }
     public void loadWidgets(Long count){
-         count2 = count;
         categoriesref.addListenerForSingleValueEvent (new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -105,8 +126,6 @@ public class DashboardFragment extends Fragment {
         });
         viewModel = new ViewModelProvider(requireActivity()).get(ItemViewModel.class);
         for (int i = 1; i <= count; i++){
-            System.out.println(allProducts.child(Integer.toString(i)));
-
             allProducts.child(Integer.toString(i)).child("quantity").addValueEventListener(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -114,7 +133,6 @@ public class DashboardFragment extends Fragment {
                     sum += Integer.parseInt(snapshot.getValue().toString());
                     dashboardQuantity.setText(String.valueOf(sum));
                     itemsNum.setText(String.valueOf(count));
-
                 }
 
                 @Override
@@ -140,4 +158,51 @@ public class DashboardFragment extends Fragment {
         }
 
     }
+    public void exportDB(){
+        BDref.child("productCategories").child("CAT0"+ counter).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                StringBuilder data = new StringBuilder();
+                data.append("Product Code,Description,Name,Price,Quantity");
+                for(int i=1; i<=snapshot.getChildrenCount(); i++){
+                    data.append("\n").append(snapshot.child(Integer.toString(i)).child("code").getValue().toString()).append(",").
+                            append(snapshot.child(Integer.toString(i)).child("description").getValue().toString()).append(",").
+                            append(snapshot.child(Integer.toString(i)).child("name").getValue().toString()).append(",").
+                            append(snapshot.child(Integer.toString(i)).child("price").getValue().toString()).append(",").
+                            append(snapshot.child(Integer.toString(i)).child("quantity").getValue().toString());
+
+                }
+
+                try{
+                    FileOutputStream out = getContext().openFileOutput("data.csv", Context.MODE_PRIVATE);
+                    out.write((data.toString()).getBytes());
+                    out.close();
+
+                    Context context = getContext();
+                    File fileLocation = new File(getContext().getFilesDir(), "backup."+ year + month + day + ".csv");
+                    Uri path = FileProvider.getUriForFile(context, "com.sc703.gualmarsh.FileProvider", fileLocation);
+                    Intent fileIntent = new Intent(Intent.ACTION_SEND);
+                    fileIntent.setType("text/csv");
+                    fileIntent.putExtra(Intent.EXTRA_SUBJECT, "DBBackup");
+                    fileIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    fileIntent.putExtra(Intent.EXTRA_STREAM, path);
+                    getContext().startActivity(Intent.createChooser(fileIntent, "Open with"));
+
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+    }
+
+
 }
