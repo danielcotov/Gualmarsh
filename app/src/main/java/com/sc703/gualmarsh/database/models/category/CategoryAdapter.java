546b22922114f509df23cd07f58891fe99b9c001
Fabián Bolaños <>
2021-04-26 21:53:35 -0600

    PNG Image conversion v3

--- app/src/main/java/com/sc703/gualmarsh/database/models/category/CategoryAdapter.java
+++ app/src/main/java/com/sc703/gualmarsh/database/models/category/CategoryAdapter.java
@@ -206,13 +206,13 @@ public class CategoryAdapter extends FirebaseRecyclerAdapter<Category, CategoryA
     }
 
     public void loadImages(Context context, ImageView imvImage, String code) {
-        String cachePath = context.getCacheDir().getAbsolutePath() + File.separator + code + ".png";
+        String cachePath = context.getCacheDir().getAbsolutePath() + File.separator + code + ".jpg";
         File cacheFile = new File(cachePath);
         if (!cacheFile.exists()) {
-            storage = FirebaseStorage.getInstance().getReference().child("Resources/Categories/" + code + ".png");
+            storage = FirebaseStorage.getInstance().getReference().child("Resources/Categories/" + code + ".jpg");
             File localFile = null;
             try {
-                localFile = new File(context.getCacheDir(), code + ".png");
+                localFile = new File(context.getCacheDir(), code + ".jpg");
             } catch (Exception e) {
                 e.printStackTrace();
             }
